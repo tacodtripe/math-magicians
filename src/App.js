7674a23c4902c62f8ed571093ea7a8aa10071111
Raul Figueroa <rfigueroa17@hotmail.com>
2021-12-03 15:41:43 -0700

    modify app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Calculator from './components/calculator';
 
-function App() {
-  return (
-    <div className="container">
-      <h1 className="row justify-content-center">Math Magicians</h1>
-      <Calculator />
-    </div>
-  );
+class App {
+  render() {
+    return (
+      <div className="container">
+        <h1 className="row justify-content-center">Math Magicians</h1>
+        <Calculator />
+      </div>
+    );
+  }
 }
 
 export default App;
