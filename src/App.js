a5a81c231aaca47858016c1b0a2482db2fcffd48
Raul Figueroa <rfigueroa17@hotmail.com>
2021-12-09 22:53:36 -0700

    add basic layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
 import Calculator from './components/calculator';
+import Home from './components/Home';
+import Quotes from './components/Quotes';
 
 const App = () => (
   <div className="container">
-    <h1 className="row justify-content-center">Math Magicians</h1>
-    <Calculator />
+    <nav className="row">
+      <h1 className="col-6 text-center">Math Magicians</h1>
+      <div className="col-6 row justify-content-between align-content-center">
+        <Link className="col text-center" to="/">Home</Link>
+        <Link className="col text-center" to="/calculator">Calculator</Link>
+        <Link className="col text-center" to="/quotes">Quotes</Link>
+      </div>
+      <Routes>
+        <Route path="calculator" element={<Calculator />} />
+        <Route path="/" element={<Home />} />
+        <Route path="quotes" element={<Quotes />} />
+      </Routes>
+    </nav>
   </div>
 );
 
---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+/* eslint-disable max-len */
+import React from 'react';
+
+const Home = () => (
+  <div className="row">
+    <h1 className="col-12">Welcome to our page!</h1>
+    <p className="col-12">
+      Quis quis anim reprehenderit voluptate ex pariatur Lorem qui occaecat. Tempor velit do nisi ut incididunt. Aute minim aliquip qui Lorem eiusmod aliqua.
+      Occaecat duis qui non enim veniam enim incididunt cillum officia non sit aute sint dolore. Tempor duis occaecat et incididunt officia dolor minim pariatur. Ut sunt nulla amet aliqua.
+      Aute excepteur consectetur ipsum ipsum labore id reprehenderit cupidatat ex consequat laborum occaecat. Ullamco qui ipsum ipsum sit esse ea quis. Eiusmod magna ipsum ad qui anim esse est est Lorem nisi velit pariatur. Deserunt aliqua aute enim Lorem et adipisicing tempor duis nisi proident. Esse laboris aliqua dolor minim sint enim ipsum nulla. Ex ea nostrud non amet elit in amet excepteur officia.
+    </p>
+    <p className="col-12">
+      Id veniam veniam qui commodo amet proident id id minim sit labore officia anim. Minim duis occaecat dolor minim non ipsum enim culpa. Officia laborum esse excepteur anim ad reprehenderit excepteur ipsum enim. Nulla consequat magna est nisi nisi mollit ea veniam in ipsum veniam.
+    </p>
+  </div>
+);
+
+export default Home;
