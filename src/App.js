b06beb9092476320c79aa4e750f22b7c637aa65a
Raul Figueroa <rfigueroa17@hotmail.com>
2021-12-10 12:57:58 -0700

    add styling

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import { Routes, Route, Link } from 'react-router-dom';
 import Calculator from './components/calculator';
 import Home from './components/Home';
 import Quotes from './components/Quotes';
+import './App.css';
 
 const App = () => (
   <div className="container">
-    <nav className="row">
-      <h1 className="col-6 text-center">Math Magicians</h1>
-      <div className="col-6 row justify-content-between align-content-center">
-        <Link className="col text-center" to="/">Home</Link>
-        <Link className="col text-center" to="/calculator">Calculator</Link>
-        <Link className="col text-center" to="/quotes">Quotes</Link>
+    <nav className="">
+      <div className="row justify-content-center nav-container">
+        <h1 className="col-5 text-center">Math Magicians</h1>
+        <div className="col-7 row justify-content-between align-content-center">
+          <Link className="col text-center nav-link" to="/">Home</Link>
+          <Link className="col text-center nav-link" to="/calculator">Calculator</Link>
+          <Link className="col text-center nav-link" to="/quotes">Quotes</Link>
+        </div>
       </div>
       <Routes>
         <Route path="calculator" element={<Calculator />} />
